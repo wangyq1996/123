f9c2725d76127d896228ba8b4d05d87a407e989b
Yuqi Wang <wangyq961129@outlook.com>
2021-02-24 17:22:54 -0500

    Guess My Number v1.0 Finished

--- JS_Udemy/section7/script.js
+++ JS_Udemy/section7/script.js
@@ -3,20 +3,47 @@
 //console.log(document.querySelector('.message').textContent);
 
 const getRandomInt = (max) => {
-    return Math.floor(1+Math.random()*Math.floor(max));
-}
+    return Math.floor(1 + Math.random() * Math.floor(max));
+};
 
 const num = getRandomInt(20);
+let highScore = 0;
 console.log(num);
-
-let userInput = 0;
+let score = 20;
+const message = document.getElementById('message');
 
 const btnClick = () => {
-    userInput = parseInt(document.getElementById('userinput').value);
-    if(!userInput) alert('No input!');
-}
+    const userInput = parseInt(document.getElementById('userinput').value);
 
-if(userInput === num) {
-    document.getElementsByClassName('.message').value = '🤘🏾Correct Number!';
-}
+    if (!userInput || userInput < 1 || userInput > 20) {
+        alert('Illegal Input!');
+        location.reload();
+    }
 
+    if (userInput === num) {
+        message.innerHTML = '🤘🏾Correct Number!';
+        document.getElementById('btnCheck').style.opacity = '0';
+        document.body.style.backgroundColor = '#60b347';
+        if (score > highScore) {
+            highScore = score;
+            document.getElementById('highscore').innerHTML = highScore;
+        }
+    } else {
+        score--;
+        if (userInput > num) message.innerHTML = 'Too High!';
+        else message.innerHTML = 'Too Low!';
+        document.getElementById('score').innerHTML = score;
+    }
+    if (score === 0) {
+        alert('Seriously?!');
+        location.reload();
+    }
+};
+
+const reset = () => {
+    document.body.style.backgroundColor = '#222';
+    message.innerHTML = 'Start guessing...';
+    score = 20;
+    document.getElementById('score').innerHTML = score;
+    document.getElementById('userinput').value = '';
+};
