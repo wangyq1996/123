2fe9513e2f15a17c8200070e7cb3742cf0f912ae
Yuqi Wang <wangyq961129@outlook.com>
2021-05-09 20:06:41 -0400

    React Courses

---
+++ React_Udemy/components/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
